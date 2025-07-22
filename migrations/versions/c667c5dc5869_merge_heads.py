d8845bcb0626ba5ac79a445790b9aa5b08667f59
Laura F <fandinorlaurad203@gmail.com>
2025-07-22 15:23:13 -0500

    ajustes en migraciones

---
+++ migrations/versions/c667c5dc5869_merge_heads.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: c667c5dc5869
+Revises: 2eebefc193e6, 5be0096fdc54
+Create Date: 2025-07-22 15:21:44.849929
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c667c5dc5869'
+down_revision = ('2eebefc193e6', '5be0096fdc54')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
